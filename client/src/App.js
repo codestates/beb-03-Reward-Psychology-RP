effbf99a17920f56e753bae2528942dc687077ed
lhj6102 <lhj6102@gmail.com>
2022-04-21 13:14:43 +0900

    fix:footer 초기작업

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import NavBar from "./component/NavBar";
 
 function App() {
-    return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Routes></Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/component/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const TopFix = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  z-index: 99;
+  top: 0ps;
+  width: 100%;
+`;
