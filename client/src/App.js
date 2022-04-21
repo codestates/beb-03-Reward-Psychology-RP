ada3c7343d49963c267330deb2facdfde8d458fe
lhj6102 <lhj6102@gmail.com>
2022-04-21 16:01:29 +0900

    feat: NavBar with logged in status

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,22 @@ import NavBar from "./component/NavBar";
 import Footer from "./component/Footer";
 import Main from "./page/main";
 import Mypage from "./page/mypage";
-import NewPost from "./page/newpost";
-import ReadPost from "./page/readpost";
+import NewPost from "./page/newPost";
+import ReadPost from "./page/readPost";
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/newpost" element={<NewPost />} />
-        <Route path="/readpost" element={<ReadPost />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar isLoggedIn={true} />
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/mypage" element={<Mypage />} />
+                <Route path="/newpost" element={<NewPost />} />
+                <Route path="/readpost" element={<ReadPost />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -1,17 +1,7 @@
 import React from "react";
-import styled from "styled-components";
-
-const TopFix = styled.div`
-  display: flex;
-  flex-direction: column;
-  position: fixed;
-  z-index: 99;
-  top: 0ps;
-  width: 100%;
-`;
 
 function Footer() {
-  return <div></div>;
+    return <div></div>;
 }
 
 export default Footer;
