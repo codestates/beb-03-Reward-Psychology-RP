6436b13cc32f70a6091d6258e710c4c7851185b0
AndrewKim330 <rendezvous330@gmail.com>
2022-04-21 12:37:40 +0900

    Routing front-end sturcture

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './component/NavBar';
+import Footer from './component/Footer';
+import Main from './page/main';
+import Mypage from './page/mypage';
+import NewPost from './page/newpost';
+import ReadPost from './page/readpost';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/mypage" element={<Mypage />} />
+                <Route path="/newpost" element={<NewPost />} />
+                <Route path="/readpost" element={<ReadPost />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
