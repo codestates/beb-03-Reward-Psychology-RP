d52cac61039b7dc789e734322c4868e14b353a6c
lhj6102 <lhj6102@gmail.com>
2022-04-22 17:17:28 +0900

    feat; postSummary component

--- client/src/page/main.js
+++ client/src/page/main.js
@@ -1,8 +1,82 @@
-import React from 'react';
+import * as React from "react";
+
+// import styled from "styled-components";
+
+import PostSummary from "../component/postSummary";
+
+//mui
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+
+// const PostContainer = styled.ul`
+//     top: 15%;
+//     left: 32%;
+//     width: 55%;
+//     height: 100%;
+
+//     background-color: green;
+//     justify-content: center;
+//     position: absolute;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: flex-start;
+//     align-items: flex-start;
+// `;
+
+const PostContainer = styled(Box)(({ theme }) => ({
+    position: "aboslute",
+    display: "flex",
+    flexDirection: "column",
+    boxShadow: 3,
+    top: "200px",
+    left: "26%",
+}));
 
 function main() {
-    return <div>Main Page!</div>;
-    
+    return (
+        <PostContainer>
+            <Stack spacing={2}>
+                <PostSummary
+                    writer="me"
+                    createdAt="20205170623"
+                    title="testTitle1111"
+                    contents="test contents test contents test contents test contents test contents"
+                    likesCount={11}
+                    commentsCount={16}
+                    postURL="/"
+                />
+                <PostSummary
+                    writer="me"
+                    createdAt="20205170623"
+                    title="testTitle2222"
+                    contents="contents test test contents test contents test contents test contentstest contents"
+                    likesCount={11}
+                    commentsCount={16}
+                    postURL="/"
+                />
+                <PostSummary
+                    writer="me"
+                    createdAt="20205170623"
+                    title="testTitle3333"
+                    contents="test contents test contents test contents test contents test contents test contents test contents "
+                    likesCount={11}
+                    commentsCount={19}
+                    postURL="/"
+                />
+                <PostSummary
+                    writer="me"
+                    createdAt="20205170623"
+                    title="testTitle4444"
+                    contents="test contents test contents test contents test contents test contents test contents test contents "
+                    likesCount={112}
+                    commentsCount={191}
+                    postURL="/"
+                />
+            </Stack>
+        </PostContainer>
+    );
 }
 
 export default main;
