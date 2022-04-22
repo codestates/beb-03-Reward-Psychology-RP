07c8ded9b0b46955227ae3901dad32be2d118b22
vamprodo47 <vamprodo47@gmail.com>
2022-04-22 17:28:41 +0900

    done: reconstruct routers & controllers

--- backend/src/routers/globalRouter.js
+++ backend/src/routers/globalRouter.js
@@ -1,21 +1,14 @@
 import express from "express";
+import { join, login } from "../controllers/userController";
+import { home, search, upload } from "../controllers/postingController";
 
 const globalRouter = express.Router();
-
-const home = (req, res) => {
-  return res.send("Home");
-};
-
-const join = (req, res) => {
-  return res.send("Join");
-};
-
-const login = (req, res) => {
-  return res.send("Login");
-};
 
 globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
+globalRouter.get("/search", search);
+globalRouter.get("/upload", upload);
+globalRouter.post("/upload", upload);
 
 export default globalRouter;
