a585b964591851c443cb39ae5bec7e13ac47492f
vamprodo47 <vamprodo47@gmail.com>
2022-04-25 15:01:22 +0900

    done: modified postingController.js & userController.js

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,7 +2,7 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
-  return res.render("Join");
+  return res.send("Join");
 };
 
 export const postJoin = async (req, res) => {
@@ -11,10 +11,12 @@ export const postJoin = async (req, res) => {
     $or: [{ email }, { userName }],
   });
 
-  if (userNameOrEmailExists) {
-    return res
-      .status(400)
-      .render("Join", { errorMessage: "This user is already registered." });
+  if (password !== password2) {
+    return res.status(400).send("Password Confirmation does not match");
+  }
+  if (!userNameOrEmailExists) {
+    return res.status(400).send("Join Error: This user is already registered");
+    // .render("Join", { errorMessage: "This user is already registered." });
   }
 
   try {
@@ -23,14 +25,17 @@ export const postJoin = async (req, res) => {
       userName,
       password,
     });
-    return res.render("Login");
+    return res.send("to Login Page, Success Join");
+    // .render("Login");
   } catch (error) {
-    return res.status(400).render("Join", { errorMessage: "Not Available" });
+    return res.status(400).send("Join Not Available");
+    // .render("Join", { errorMessage: "Not Available" });
   }
 };
 
 export const getLogin = async (req, res) => {
-  return res.render("Login");
+  return res.send("Login Page");
+  // .render("Login");
 };
 
 export const postLogin = async (req, res) => {
@@ -38,14 +43,12 @@ export const postLogin = async (req, res) => {
   const user = await User.findOne({ userName });
   const passwordCompare = bcrypt.compare(password, user.password);
   if (!user) {
-    return res.status(404).render("Login", {
-      errorMessage: "An account with this username dose not exist.",
-    });
+    return res.status(404).send("Post Login Success");
+    // .render("Login", {errorMessage: "An account with this username dose not exist.",});
   }
   if (!passwordCompare) {
-    return res.status(404).render("Login", {
-      errorMessage: "Wrong Password",
-    });
+    return res.status(404).send("Password Comparision does not match");
+    // .render("Login", {errorMessage: "Wrong Password",});
   }
   console.log("🙆‍♂️ LOG USER IN!");
   return res.redirect("/");
