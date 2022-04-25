94a23aa88df26eeb2d6e78df3449652c24533164
vamprodo47 <vamprodo47@gmail.com>
2022-04-25 10:21:28 +0900

    done: postingController

--- backend/src/controllers/postingController.js
+++ backend/src/controllers/postingController.js
@@ -1,21 +1,99 @@
 import Posting from "../models/Posting";
 
 export const home = async (req, res) => {
-  await Posting.find();
+  try {
+    const postings = await Posting.find({}).sort({ createdAt: "desc" });
+    return res.render("Home", { postings });
+  } catch (error) {
+    return res.send("This is not the web page you are looking for!");
+  }
 };
 
-export const search = (req, res) => {
-  return res.send("Search");
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let postings = [];
+  if (keyword) {
+    postings = await Posting.find({
+      title: { $regex: new RegExp(keyword, i) },
+    });
+  }
+  return res.render("search", postings);
 };
 
-export const upload = (req, res) => {
-  return res.send("Upload");
+export const getUpload = (req, res) => {
+  return res.render("Upload");
 };
 
-export const watch = (req, res) => {
-  return res.send("Watch");
+export const postUpload = async (req, res) => {
+  // const postingSchema = new mongoose.Schema({
+  //   userId: String,
+  //   title: { type: String, trim: true, maxlength: 50 },
+  //   createdAt: { type: Date, default: Date.now },
+  //   contents: { type: String, trim: true, minlength: 20 },
+  //   hashtags: [{ type: String, trim: true }],
+  //   meta: {
+  //     voting: { type: Number, default: 0 },
+  //     views: { type: Number, default: 0 },
+  //     comments: { type: Number, default: 0 },
+  //   },
+  // });
+  const { title, contents, hashtags } = req.body;
+  try {
+    await Posting.create({
+      title,
+      contents,
+      hashtags: Posting.formatHashtags(hashtags),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    console.log("❌ Post Upload Error:", error);
+    return res.status(400).render("Upload");
+  }
 };
 
-export const edit = (req, res) => {
-  return res.send("Edit");
+export const watch = async (req, res) => {
+  const {id} =req.params;
+  const posting = Posting.findById(id);
+
+  if(!posting){
+    console.log("404 Not Found!");
+    return res.status(404).render("404");
+  }
+
+  return res.render("Watch", posting)
+};
+
+export const getEdit = (req, res) => {
+  const {id} = req.params;
+  const posting = await Posting.findById(id);
+  if(!posting) {
+    console.log("404 Not Found!");
+    return res.status(404).render("404");
+  }
+  return res.render("Edit", posting);
+};
+
+export const postEdit = async (req, res) => {
+  const {id} = req.params;
+  const posting = await Posting.exists({_id:id});
+  const {title, contents, hashtags} = req.body;
+
+  if(!posting) {
+    console.log("404 Not Found!");
+    return res.status(404).render("404");
+  }
+
+  await Posting.findByIdAndUpdate(id,{
+    title,
+    contents,
+    hashtags: Posting.formatHashtags(hashtags)
+  })
+};
+
+export const deletePosting = async (req, res) => {
+  const { id } = req.body;
+  
+  await Posting.findByIdAndDelete(id);
+
+  return res.redirect("/");
 };
